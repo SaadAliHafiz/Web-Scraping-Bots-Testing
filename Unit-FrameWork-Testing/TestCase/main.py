1d55ecbf872111d92657141f06fbe44a010bc834
Saad Ali <59177839+SaadAliHafiz@users.noreply.github.com>
2022-06-17 00:11:12 +0500

    Created first Test

--- Unit-FrameWork-Testing/TestCase/main.py
+++ Unit-FrameWork-Testing/TestCase/main.py
@@ -1,13 +1,23 @@
 import unittest
-from selenium import webdriver
 import page
+from selenium import webdriver
+from page import SearchTextElement
+from page import SearchResultPage
 
 class PythonOrgSearch(unittest.TestCase):
+    
     def setUp(self):
         self.driver = webdriver.Chrome("C:\Program Files (x86)\chromedriver.exe")
         self.driver.get("https://www.python.org/")
         
         
+    def test_search_python(self):
+        mainPage=page.MainPage(self.driver)
+        assert mainPage.is_title_matches()
+        mainPage.search_text_element ="pycon"
+        mainPage.click_go_button()
+        search_result_page=page.SearchResultPage(self.driver)
+        assert search_result_page.is_results_found()
     # def test_example(self):
     #     print("test")
     #     assert False    
@@ -19,9 +29,9 @@ class PythonOrgSearch(unittest.TestCase):
     # def not_a_test(self):
     #     print("this dont print automatically")
         
-    def test_title(self):
-        mainPage=page.MainPage()
-        assert mainPage.is_title_matches()
+    # def test_title(self):
+    #     mainPage=page.MainPage()
+    #     assert mainPage.is_title_matches()
     def tearDown(self):
         self.driver.close()
         
